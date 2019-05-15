b9b0ad89ca3525f4e96c254e318387058ec90d5a
cosmoskey <barskoidim@gmail.com>
2019-05-15 18:19:56 +0300

    Add methods findByName, create(by skillName) to SkillsRepository

--- src/main/java/app/repositories/SkillsRepository.java
+++ src/main/java/app/repositories/SkillsRepository.java
@@ -4,6 +4,13 @@ import app.entities.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SkillsRepository extends JpaRepository<Skill, Long> {
+    Optional<Skill> findByName(String name);
+
+    default Skill create(String skillName) {
+        return save(new Skill(skillName));
+    }
 }
