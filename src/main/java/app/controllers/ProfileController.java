698dfa31ac8a73e069c3553d84637071bfab18ba
cosmoskey <barskoidim@gmail.com>
2019-05-15 18:30:15 +0300

    Refactor code in ProfileController

--- src/main/java/app/controllers/ProfileController.java
+++ src/main/java/app/controllers/ProfileController.java
@@ -1,10 +1,11 @@
 package app.controllers;
 
 import app.entities.*;
+import app.exceptions.AccountNotFoundException;
 import app.forms.EducationForm;
-import app.repositories.*;
 import app.services.AccountDetails;
 import app.services.EducationService;
+import app.services.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -14,18 +15,12 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping(path = "/profile")
 public class ProfileController {
-    private final ProfileRepository profileRepository;
-    private final SkillsRepository skillsRepository;
-    private final WorkExperienceRepository workExperienceRepository;
-    private final ProfileSkillRepository profileSkillRepository;
+    private final ProfileService profileService;
     private final EducationService educationService;
 
     @Autowired
-    public ProfileController(ProfileRepository profileRepository, SkillsRepository skillsRepository, EducationRepository educationRepository, WorkExperienceRepository workExperienceRepository, ProfileSkillRepository profileSkillRepository, EducationService educationService) {
-        this.profileRepository = profileRepository;
-        this.skillsRepository = skillsRepository;
-        this.workExperienceRepository = workExperienceRepository;
-        this.profileSkillRepository = profileSkillRepository;
+    public ProfileController(ProfileService profileService, EducationService educationService) {
+        this.profileService = profileService;
         this.educationService = educationService;
     }
 
@@ -33,8 +28,10 @@ public class ProfileController {
     public String getProfileByIdPage(@PathVariable Long id,
                                      ModelMap modelMap,
                                      @AuthenticationPrincipal AccountDetails account) {
-        final Profile profile = profileRepository.findById(id).get();
-        final Account user = profileRepository.findById(account.getUser().getId()).get();
+        final Profile profile = profileService.findById(id)
+                .orElseThrow(AccountNotFoundException::new);
+        final Account user = profileService.findAccountById(account.getUser().getId())
+                .orElseThrow(AccountNotFoundException::new);
         modelMap.put("profile", profile);
         modelMap.put("user", user);
         return "profile";
@@ -43,15 +40,22 @@ public class ProfileController {
     @RequestMapping(method = RequestMethod.GET)
     public String getProfilePage(@AuthenticationPrincipal AccountDetails account, ModelMap modelMap) {
         final Account user = account.getUser();
-        final Profile profile = profileRepository.findById(user.getId()).get();
+        final Profile profile = profileService.findById(user.getId())
+                .orElseThrow(AccountNotFoundException::new);
         modelMap.put("profile", profile);
         modelMap.put("user", profile);
         return "profile";
     }
 
     @RequestMapping(path = "/{id}/edit", method = RequestMethod.POST)
-    public String editStatus(@AuthenticationPrincipal AccountDetails account, String status, String location) {
-        final Profile profile = profileRepository.findById(account.getUser().getId()).get();
+    public String editStatus(@AuthenticationPrincipal AccountDetails account,
+                             String status,
+                             String location,
+                             @PathVariable long id) {
+        final Profile profile = profileService.findById(account.getUser().getId())
+                .orElseThrow(AccountNotFoundException::new);
+        assert profile.getId() == id;
+
         String newStatus = "";
         if (status != null) {
             switch (Integer.parseInt(status)) {
@@ -73,44 +77,45 @@ public class ProfileController {
         }
         profile.setStatus(newStatus);
         profile.setLocation(location);
-        profileRepository.save(profile);
+        profileService.save(profile);
         return "redirect:/profile";
     }
 
     @PostMapping(path = "/{id}/add_skill")
-    public String addSkill(@AuthenticationPrincipal AccountDetails account, String skillName) {
-        Skill skill = new Skill();
-        skill.setName(skillName);
-        skillsRepository.save(skill);
+    public String addSkill(@AuthenticationPrincipal AccountDetails account,
+                           String skillName,
+                           @PathVariable long id) {
+        Skill skill = profileService.createSkill(skillName);
 
-        final Profile profile = profileRepository.findById(account.getUser().getId()).get();
+        final Profile profile = profileService.findById(account.getUser().getId())
+                .orElseThrow(AccountNotFoundException::new);
+        assert profile.getId() == id;
 
-        ProfileSkill profileSkill = new ProfileSkill();
-        profileSkill.setProfile(profile);
-        profileSkill.setSkill(skill);
-        profileSkillRepository.save(profileSkill);
-
-        profile.getSkills().add(profileSkill);
-        profileRepository.save(profile);
+        profileService.add(skill).to(profile);
         return "redirect:/profile";
     }
 
     @PostMapping(path = "/{id}/add_edu")
-    public String addEducation(@AuthenticationPrincipal AccountDetails account, EducationForm educationForm) {
-        final Profile profile = profileRepository.findById(account.getUser().getId()).get();
-        final Education education = educationService.save(educationForm, profile);
-        profile.getEducations().add(education);
-        profileRepository.save(profile);
+    public String addEducation(@AuthenticationPrincipal AccountDetails account,
+                               EducationForm educationForm,
+                               @PathVariable long id) {
+        final Profile profile = profileService.findById(account.getUser().getId())
+                .orElseThrow(AccountNotFoundException::new);
+        assert profile.getId() == id;
+
+        educationService.save(educationForm, profile);
         return "redirect:/profile";
     }
 
     @PostMapping(path = "/{id}/add_work")
-    public String addWorkExperience(@AuthenticationPrincipal AccountDetails account, WorkExperience workExperience) {
-        final Profile profile = profileRepository.findById(account.getUser().getId()).get();
-        profile.getWorkExperiences().add(workExperience);
-        workExperienceRepository.save(workExperience);
-        profileRepository.save(profile);
+    public String addWorkExperience(@AuthenticationPrincipal AccountDetails account,
+                                    WorkExperience workExperience,
+                                    @PathVariable long id) {
+        final Profile profile = profileService.findById(account.getUser().getId())
+                .orElseThrow(AccountNotFoundException::new);
+        assert profile.getId() == id;
+
+        profileService.add(workExperience).to(profile);
         return "redirect:/profile";
     }
-
 }
