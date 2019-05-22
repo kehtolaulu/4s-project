a3f2a7961abfc1a7ff4754a58fef2282567b158f
cosmoskey <barskoidim@gmail.com>
2019-05-22 18:14:59 +0300

    Fix errors in neo4j repository (cannot use depth as a parameter), ensure profile existence before linking contacts

--- src/main/java/app/repositories/neo4j/Neo4jProfileRepository.java
+++ src/main/java/app/repositories/neo4j/Neo4jProfileRepository.java
@@ -5,17 +5,28 @@ import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public interface Neo4jProfileRepository extends Neo4jRepository<Neo4jProfile, Long> {
     @Query("MATCH (a:Profile {profileId: {id1}}) MATCH (b:Profile {profileId: {id2}}) CREATE (a)-[:KNOWS]->(b)")
     void createLink(@Param("id1") long profileId1, @Param("id2") long profileId2);
 
-    Neo4jProfile findByProfileId(Long profileId);
+    @Query("MATCH (p:Profile {profileId: {id}})-[k:KNOWS]->(f:Profile) RETURN p, k, f;")
+    Optional<Neo4jProfile> findByProfileId(@Param("id") Long profileId);
 
     @Query("CREATE (p:Profile {profileId: {id}}) RETURN p;")
     void createProfile(@Param("id") long profileId);
 
-    @Query("MATCH (p:Profile {profileId: {id}})-[r:KNOWS*1..{depth}]->(p1) RETURN p, r1, p1;")
-    Stream<Neo4jProfile> findCircleWithDepth(@Param("id") long profileId, @Param("depth") int depth);
+    @Query("MATCH (p:Profile {profileId: {id}})-[k:KNOWS]->(f) RETURN p, k, f;")
+    Stream<Neo4jProfile> findCircleWithDepth1(@Param("id") long profileId);
+
+    @Query("MATCH (p:Profile {profileId: {id}})-[k:KNOWS*1..2]->(f) RETURN p, k, f;")
+    Stream<Neo4jProfile> findCircleWithDepth2(@Param("id") long profileId);
+
+    @Query("MATCH (p:Profile {profileId: {id}})-[k:KNOWS*1..3]->(f) RETURN p, k, f;")
+    Stream<Neo4jProfile> findCircleWithDepth3(@Param("id") long profileId);
+
+    @Query("MATCH (p:Profile {profileId: {id}}) RETURN exists(p);")
+    boolean existsByProfileId(@Param("id") Long profileId);
 }
--- src/main/java/app/services/ContactService.java
+++ src/main/java/app/services/ContactService.java
@@ -6,6 +6,7 @@ import app.repositories.ProfileRepository;
 import app.repositories.neo4j.Neo4jProfileRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
@@ -26,13 +27,26 @@ public class ContactService {
 
     public Consumer<Profile> addContact(Profile profile1) {
         return profile2 -> {
+            // ensure that profiles exist in graph
+            for (Profile p : Arrays.asList(profile1, profile2))
+                if (!neo4jProfileRepository.existsByProfileId(p.getId())) {
+                    neo4jProfileRepository.save(new Neo4jProfile(p.getId()));
+                }
+            // create relation p1 -- :KNOWS -> p2
             neo4jProfileRepository.createLink(profile1.getId(), profile2.getId());
         };
     }
 
     public Function<Integer, List<Profile>> circleOfProfile(long profileId) {
         return depth -> {
-            final Stream<Neo4jProfile> circle = neo4jProfileRepository.findCircleWithDepth(profileId, depth);
+            final Stream<Neo4jProfile> circle;
+            if (depth == 1) {
+                circle = neo4jProfileRepository.findCircleWithDepth1(profileId);
+            } else if (depth == 2) {
+                circle = neo4jProfileRepository.findCircleWithDepth2(profileId);
+            } else {
+                circle = neo4jProfileRepository.findCircleWithDepth3(profileId);
+            }
             final Collection<Long> ids = circle.map(Neo4jProfile::getProfileId).collect(Collectors.toList());
             return profileRepository.findAllById(ids);
         };
